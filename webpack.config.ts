03c374a92aa814b60c5a4232bc7aa40218dc24fa
Сергей <konilovsergey55@gmail.com>
2023-07-24 18:05:27 +0300

    start project

--- webpack.config.ts
+++
@@ -1,8 +0,0 @@
-import webpack from "webpack";
-import { buildConfiguration } from "config/buildConfiguration";
-
-export default module.exports = ():webpack.Configuration => {
-  const config: webpack.Configuration = buildConfiguration()
-
-  return config;
-};
