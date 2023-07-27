57afde10f51e45bbb909e0bd7463475ad9da84c3
Сергей <konilovsergey55@gmail.com>
2023-07-27 18:05:42 +0300

    added jest unit test

---
+++ src/utils/increment.test.ts
@@ -0,0 +1,8 @@
+import {describe, expect, test} from '@jest/globals';
+import {increment} from './increment'
+
+describe('sum', () => {
+    test('increment', () => {
+      expect(increment(2)).toBe(4);
+    });
+  });
---
+++ src/utils/increment.ts
@@ -0,0 +1,3 @@
+export const increment = (a: number) => {
+  return a * 2;
+};
